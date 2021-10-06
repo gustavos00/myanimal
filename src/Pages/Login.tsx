6e0f81154fa2130880cd8722bc7125716150715c
Gustavo Santos <z.gsantos20@gmail.com>
2021-10-06 14:31:52 +0000

    Making the login page

--- src/Pages/Login.tsx
+++ src/Pages/Login.tsx
@@ -1,35 +1,70 @@
 import React from "react";
-import { Text, View, StyleSheet, SafeAreaView } from "react-native";
+import { Text, View, StyleSheet, TouchableOpacity, Image } from "react-native";
+import Svg, { Path } from "react-native-svg";
+import { LinearGradient } from "expo-linear-gradient"
 
 const Login = () => {
   return (
     <>
-    <SafeAreaView />
-      <View style={styles.wave}>
-        <Text>Login Wave here</Text>
+      <View style={styles.background}>
+        <LinearGradient 
+          style={styles.gradient}
+          colors={['#FFBB95', '#FFE1D0']}>
+        </LinearGradient>
+        <View style={{position: 'absolute', top: '80%', width: '100%', height: 200}}>
+          <Svg
+            height="60%"
+            width="100%"
+            viewBox="0 0 1440 320"
+            >
+            <Path
+              fill="#FFE1D0"
+              d="M0,0L26.7,53.3C53.3,107,107,213,160,218.7C213.3,224,267,128,320,122.7C373.3,117,427,203,480,245.3C533.3,288,587,288,640,288C693.3,288,747,288,800,277.3C853.3,267,907,245,960,202.7C1013.3,160,1067,96,1120,112C1173.3,128,1227,224,1280,234.7C1333.3,245,1387,171,1413,133.3L1440,96L1440,0L1413.3,0C1386.7,0,1333,0,1280,0C1226.7,0,1173,0,1120,0C1066.7,0,1013,0,960,0C906.7,0,853,0,800,0C746.7,0,693,0,640,0C586.7,0,533,0,480,0C426.7,0,373,0,320,0C266.7,0,213,0,160,0C106.7,0,53,0,27,0L0,0Z"
+            />
+          </Svg>
+        </View>
       </View>
 
       <View style={styles.buttonsContainer}>
-        <View style={styles.button}>
-          <Text>Google log-in</Text>
-        </View>
+        <TouchableOpacity activeOpacity={.7} style={[styles.button, styles.googleButton]}>
+          <Image 
+            style={styles.img}
+            source={require('../assets/img/googleLogo.png')} />
+          <Text style={styles.buttonText}>Use Google</Text>
+        </TouchableOpacity>
 
-        <View style={styles.button}>
-          <Text>Facebook log-in</Text>
-        </View>
+        <TouchableOpacity activeOpacity={.7} style={[styles.button, styles.facebookButton]}>
+          <Image 
+            style={styles.img}
+            source={require('../assets/img/facebookLogo.png')} />
+          <Text style={styles.buttonText}>Use Facebook</Text>
+        </TouchableOpacity>
       </View>
     </>
   )
 }
 
 const styles = StyleSheet.create({
-  wave: {
-    height: '60%',
+  background: {
+    height: '65%',
     width: '100%',
 
-    borderWidth: 1,
-    borderColor: 'red'
+    position: 'relative',
   },
+
+  gradient: {
+    height: '84%',
+
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+
+  img: {
+    marginRight: 10, 
+    
+    width: 30,
+    height: 30,
+  }, 
 
   button: {
     width: 280,
@@ -38,12 +73,27 @@ const styles = StyleSheet.create({
     
     paddingRight: 18,
     paddingLeft: 18,
-    paddingTop: 10,
-    paddingBottom: 10,
+    paddingTop: 15,
+    paddingBottom: 15,
 
     borderRadius: 5,
-    borderWidth: 1,
-    borderColor: 'green'
+    
+    flexDirection: 'row',
+    alignItems: 'center'
+  },
+
+  buttonText: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    color: 'white',
+  },
+
+  googleButton: {
+    backgroundColor: '#CE4232',
+  },
+
+  facebookButton: {
+    backgroundColor: '#3C66C4',
   },
 
   buttonsContainer: {
@@ -51,9 +101,6 @@ const styles = StyleSheet.create({
 
     justifyContent: 'center',
     alignItems: 'center',
-
-    borderWidth: 1,
-    borderColor: 'blue'
   }
 })
 
