88bd27f91f6ba36190f340b56d426defd1c3d593
Gustavo Santos <z.gsantos20@gmail.com>
2021-10-09 20:44:45 +0000

    Creating a splash screen

--- src/navigator/MainStack.tsx
+++ src/navigator/MainStack.tsx
@@ -4,10 +4,12 @@ import { NavigationContainer } from '@react-navigation/native';
 
 import Login from "../pages/Login";
 import Home from "../pages/Home"
+import SplashScreen from '../pages/SplashScreen';
 
 export type RootStackParamList = {
   Login: undefined;
   Home: undefined;
+  SplashScreen: undefined,
 }
 
 const {Navigator, Group, Screen} = createStackNavigator<RootStackParamList>()
@@ -19,6 +21,7 @@ export default () => {
         <Group>
           <Screen options={{headerShown: false}} name="Login" component={Login}/>
           <Screen options={{headerShown: false}} name="Home" component={Home}/>
+          <Screen options={{headerShown: false}} name="SplashScreen" component={SplashScreen}/>
         </Group>
       </Navigator>
     </NavigationContainer>
