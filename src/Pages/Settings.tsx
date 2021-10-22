641c59cf8e787196e2e903437e127f209cb7fd9a
Gustavo Santos <z.gsantos20@gmail.com>
2021-10-22 11:22:47 +0000

    Receiving params and showing

--- src/Pages/Settings.tsx
+++ src/Pages/Settings.tsx
@@ -1,25 +1,27 @@
 import React from 'react';
 import { RouteProp, useRoute } from '@react-navigation/native';
+import { RootStackParamList } from '../navigator/MainStack';
 
 import Background from '../components/Background';
 import BackgroundHeader from '../components/BackgroundHeader';
 import Header from '../components/Header';
-import { RootStackParamList } from '../navigator/MainStack';
+import Footer from '../components/Footer';
 
 function Settings() {
   const { params } = useRoute<RouteProp<RootStackParamList, 'Settings'>>();
-  console.log(params.name)
+  
   return (
     <>
       <Header name={params.name} image={params.photo}/>
 
       <Background>
         <>
-          <BackgroundHeader text={'Your animals'} />
+          <BackgroundHeader text={'Settings'} />
 
-        
         </>        
       </Background>
+
+      <Footer />
     </>
   );
 }
