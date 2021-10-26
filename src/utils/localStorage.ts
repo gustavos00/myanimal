b2e2602627a27bb23f2190c241de840e0796ff8c
Gustavo Santos <z.gsantos20@gmail.com>
2021-10-26 01:44:47 +0000

    Creating a function to get localstorage items

--- src/utils/localStorage.ts
+++ src/utils/localStorage.ts
@@ -1,9 +1,8 @@
 import * as SecureStore from 'expo-secure-store';
 
-export async function getStorageItem(key: string) {
-
-
-  return 'testando essa buceta';
+export async function getStorageItem(value: string) {
+  let data = await SecureStore.getItemAsync(value);
+  return data;
 }
 
 export async function setStorageItem(key: string, value: string) {
