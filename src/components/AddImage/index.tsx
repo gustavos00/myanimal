453ba3fca5297f18a94a64676c56d421025837d8
Gustavo Santos <z.gsantos20@gmail.com>
2021-10-29 23:21:09 +0000

    Creating the necessary componentes

---
+++ src/components/AddImage/index.tsx
@@ -0,0 +1,62 @@
+import React from 'react';
+import { Image, StyleSheet, View, TouchableOpacity } from 'react-native';
+
+import globalStyles from '../../assets/styles/global';
+
+interface AddImageProps {
+  widthSize?: string,
+  heightSize?: string
+}
+
+function AddImage({ widthSize, heightSize }: AddImageProps) {
+  return (
+    <>
+    <View style={styles.container}>
+      <TouchableOpacity activeOpacity={.5} style={[styles.circle, widthSize ? {width: widthSize} : {width: 190}, heightSize ? {height: heightSize} : {height: 190}]}>
+        <Image style={styles.image} source={require('../../assets/img/add.png')} />
+      </TouchableOpacity>
+    </View>
+    </>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    width: '100%', 
+
+    position: 'absolute',
+    top: '7%',
+
+    elevation: 100,
+    zIndex: 1000,
+    
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+
+  circle: {
+    width: 190,    
+    height: 190,
+
+    borderRadius: 100,
+    backgroundColor: globalStyles.white,
+
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 5,
+    },
+    shadowOpacity: 0.3,
+    shadowRadius: 3,
+
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+
+  image: {
+    width: '30%',
+    height: '30%',
+  }
+})
+
+export default AddImage;
---
+++ src/components/Input/index.tsx
@@ -0,0 +1,64 @@
+import React, { ReactNode } from 'react';
+import { StyleSheet, Text, TextInput, View } from 'react-native';
+import globalStyles from '../../assets/styles/global';
+import Background from '../Background/index';
+
+interface InputProps {
+  handleChangeFunction: (e: string) => void
+  text: string
+}
+
+function Input({ handleChangeFunction, text }: InputProps) {
+  return (
+    <>
+      <View style={styles.container}>
+        <View style={styles.textContainer}>
+          <Text style={styles.text}>{text}</Text>
+        </View>
+
+        <TextInput onChangeText={text => handleChangeFunction(text)} style={styles.input} />
+      </View>
+    </>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    paddingTop: 10,
+    paddingBottom: 10,
+    paddingLeft: 5,
+    paddingRight: 5,
+
+    marginTop: 10,
+    marginBottom: 10,
+
+    borderWidth: 1,
+    borderColor: globalStyles.gray
+  },
+
+  input: {
+    padding: 10
+  },
+
+  text: {
+    paddingLeft: 10,
+    paddingRight: 10,
+
+    fontSize: 14,
+    color: globalStyles.darkGray
+  },
+
+  textContainer: {
+    padding: 2,
+    backgroundColor: '#fff',
+
+    position: 'absolute',
+    top: -10, 
+    left: 10,
+
+    zIndex: 10, 
+    elevation: 10
+  }
+})
+
+export default Input;
