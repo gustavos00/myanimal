1739753bf32f00cc89f8d9acc358d5174cca6e98
Gustavo Santos <z.gsantos20@gmail.com>
2021-10-29 10:26:52 +0000

    Changing API url

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: 'http://localhost:3000/api/'
+  baseURL: 'https://myanimalapi.herokuapp.com/api'
 })
 
 export default instance
