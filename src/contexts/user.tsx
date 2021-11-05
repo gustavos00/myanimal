a903c1f40a9b4ee04b0ba3712cd7c01ea88dd160
Gustavo Santos <z.gsantos20@gmail.com>
2021-11-05 01:05:48 -0100

    Creating a interface to animalData and adding push function

--- src/contexts/user.tsx
+++ src/contexts/user.tsx
@@ -1,11 +1,21 @@
 import React, { createContext, useEffect, useState } from "react";
 import * as auth from '../services/auth'
 
+interface AnimalData {
+  age: string,
+  chipnumber: string,
+  name: string,
+  photo: string,
+  race: string,
+  userid: string,
+}
+
 interface UserContextData {
   familyname: string,
   givenname: string,
   email: string,
   photourl: string,
+  animalData: Array<AnimalData>,
 }
 
 interface AuthContextData {
@@ -13,11 +23,11 @@ interface AuthContextData {
   token: string | void,
   user: UserContextData | void,
 
-  googleSignIn(): void,
+  googleSignIn: () => Promise<boolean>,
+  pushAnimalData: (data : AnimalData) => void,
 }
 
 const AuthContext = createContext<AuthContextData>({} as AuthContextData);
-
 
 export function AuthProvider({children} : any) {
   const [user, setUser] = useState<UserContextData | void>()
@@ -25,8 +35,16 @@ export function AuthProvider({children} : any) {
 
   const googleSignIn = async() => {
     const response = await auth.GoogleSignIn()
+    if(!response) return false;
+
     setToken(response.token)
     setUser(response)
+
+    return true;
+  }
+
+  const pushAnimalData = (data : AnimalData) => {
+    user?.animalData.push(data)
   }
 
   return (
@@ -35,7 +53,8 @@ export function AuthProvider({children} : any) {
       token,
       user,
 
-      googleSignIn
+      googleSignIn,
+      pushAnimalData,
     }}>
       {children}
     </AuthContext.Provider>
