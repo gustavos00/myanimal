0d6bc37864530247c6d5a50717d7e90a311fe4a9
Gustavo Santos <z.gsantos20@gmail.com>
2021-11-05 01:07:55 -0100

    Creating animal

--- src/Pages/CreateOrUpdateAnimal.tsx
+++ src/Pages/CreateOrUpdateAnimal.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useContext, useState } from 'react';
 import { StyleSheet, View } from 'react-native';
 
 import api from '../api/api';
@@ -10,49 +10,49 @@ import Button from '../components/Button';
 import CreateOrUpdateSwitch from '../components/CreateOrUpdateSwitch';
 import Footer from '../components/Footer/index';
 import Input from '../components/Input';
-import { getStorageItem } from '../utils/localStorage';
+import AuthContext from '../contexts/user';
 
 interface CreateOrUpdateAnimalProps {
   type: string;
 }
 
-interface getIdProps {
-  id: string
-}
-
-interface createAnimalProps {
-  fullname: string,
+interface CreateAnimalProps {
+  name: string,
   age: string,
   race: string,
   chipnumber: string, 
-  id: string
+  photo: string,
 }
 
 function CreateOrUpdateAnimal({ type }: CreateOrUpdateAnimalProps) {
-  const [fullname, setFullname] = useState('')
+  const [name, setName] = useState('')
   const [age, setAge] = useState('')
   const [race, setRace] = useState('')
   const [chipnumber, setChipnumber] = useState('')
   const [isEnabled, setIsEnabled] = useState<boolean>(false);
+  const { token, pushAnimalData } = useContext(AuthContext);
 
   const handleSubmitForm = async () => {
-    const token = await getStorageItem('token')
-
-    let params = new URLSearchParams({
-      token: token ?? "",
-    })
-    let res = await api.post('/user/id', params)
-    const { id } = res.data as unknown as getIdProps
-
-    params = new URLSearchParams({
-      name: fullname,
+    const params = new URLSearchParams({
+      name,
       age,
       race,
       chipnumber,
-      id
+      token: token ?? '',
+      photo: '',
     })
 
-    const result = await api.post('/animal/create', params)
+    const { data } = await api.post('/animal/create', params)
+    const animalData = {
+      name,
+      age,
+      race,
+      chipnumber,
+      userid: '',
+      photo: '',
+    }
+
+    pushAnimalData(animalData)
   }
 
   return (
@@ -63,7 +63,7 @@ function CreateOrUpdateAnimal({ type }: CreateOrUpdateAnimalProps) {
         <Background heightSize={'75%'}>
           <View style={styles.container}>
             <View style={styles.inputsContainer}> 
-              <Input handleChangeFunction={(e: string) => setFullname(e)} text={'Full Name'}/>
+              <Input handleChangeFunction={(e: string) => setName(e)} text={'Full Name'}/>
               <Input handleChangeFunction={(e: string) => setAge(e)} text={'Age'}/>
               <Input handleChangeFunction={(e: string) => setRace(e)} text={'Race'}/>
               <Input handleChangeFunction={(e: string) => setChipnumber(e)} text={'Tracking code'}/>
