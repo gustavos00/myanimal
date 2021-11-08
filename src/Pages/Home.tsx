caed18db2bea115c3dae70d722b50cb95aaeff0f
Gustavo Santos <z.gsantos20@gmail.com>
2021-11-07 23:11:45 -0100

    Changing to settings screen on animalElement

--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -8,11 +8,21 @@ import BackgroundHeader from "../components/BackgroundHeader";
 import AnimalElement from "../components/AnimalElement";
 import Footer from "../components/Footer";
 import AuthContext from "../contexts/user";
+import { useNavigation } from '@react-navigation/core';
 
 
 const Home = () => {
   const [isEditing, setIsEditing] = useState<boolean>(false);
   const { user } = useContext(AuthContext);
+  const navigation = useNavigation();
+
+  const changingToSettings = (item : Object) => {
+    navigation.navigate('CreateOrUpdateAnimal' as never, {
+      type: 'view',
+      animalInfo: item
+    } as never)
+  }
+  
 
   return (
     <>
@@ -30,7 +40,7 @@ const Home = () => {
             { user?.animalData.map((item, index) => {
               return (
                 <View key={index}>
-                  <TouchableOpacity onLongPress={() => setIsEditing(!isEditing)}>
+                  <TouchableOpacity onPress={() => changingToSettings(item)} onLongPress={() => setIsEditing(!isEditing)}>
                     <AnimalElement isEditing={isEditing} name={item.name} race={item.race} imageUrl={item.photourl} />
                   </TouchableOpacity>
                 </View>
