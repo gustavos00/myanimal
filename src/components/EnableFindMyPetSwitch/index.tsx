9610a2f62359bbadaf000705f25e069a543e471a
Gustavo Santos <z.gsantos20@gmail.com>
2021-11-11 22:18:56 -0100

    Creating new styled switch

---
+++ src/components/EnableFindMyPetSwitch/index.tsx
@@ -0,0 +1,37 @@
+import React, { ReactNode } from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+import globalStyles from '../../assets/styles/global';
+import StyledSwitch from '../StyledSwitch/index';
+
+interface EnableFindMyPetSwitchProps {
+  enableFunction?: (state : boolean) => void;
+  enableValue: boolean
+}
+
+function EnableFindMyPetSwitch({ enableFunction, enableValue }: EnableFindMyPetSwitchProps) {
+  return (
+    <>
+      <View style={styles.switchContainer}>
+        <Text style={styles.switchText}>Enable find my pet</Text>
+        <StyledSwitch enableFunction={enableFunction} enableValue={enableValue} />
+      </View>
+    </>
+  );
+}
+
+const styles = StyleSheet.create({
+  switchContainer: {
+    width: '80%',
+
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center'
+  },
+
+  switchText: {
+    color: globalStyles.darkGray
+  }
+})
+
+export default EnableFindMyPetSwitch;
