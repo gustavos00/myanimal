91f6878cfa8fcfc2e83b161cfbd295665d70ad2f
Gustavo Santos <z.gsantos20@gmail.com>
2021-11-11 22:19:47 -0100

    Changing styled switch toggle function to optional

--- src/components/StyledSwitch/index.tsx
+++ src/components/StyledSwitch/index.tsx
@@ -4,23 +4,26 @@ import { Switch } from 'react-native';
 import globalStyles from '../../assets/styles/global';
 
 interface StyledSwitchProps {
-  enableFunction: (state : boolean) => void;
+  enableFunction?: (state : boolean) => void;
   enableValue: boolean
 }
 
 function StyledSwitch({ enableFunction, enableValue }: StyledSwitchProps) {
-  const toggleSwitch = () => enableFunction(!enableValue);
+  let toggleSwitch
+  if(enableFunction) {
+    toggleSwitch = () => enableFunction(!enableValue);
+  }
 
   return (
     <>
-        <Switch
-          trackColor={{ false: globalStyles.white, true: globalStyles.mainColor }}
-          thumbColor={enableValue ? globalStyles.white : globalStyles.mainColor}
-          ios_backgroundColor={globalStyles.white}
-          onValueChange={toggleSwitch}
-          style={{ transform: [{ scaleX: .8 }, { scaleY: .8 }] }}
-          value={enableValue}
-        />
+      <Switch
+        trackColor={{ false: globalStyles.white, true: globalStyles.mainColor }}
+        thumbColor={enableValue ? globalStyles.white : globalStyles.mainColor}
+        ios_backgroundColor={globalStyles.white}
+        onValueChange={toggleSwitch}
+        style={{ transform: [{ scaleX: .8 }, { scaleY: .8 }] }}
+        value={enableValue}
+      />
     </>
   );
 }
