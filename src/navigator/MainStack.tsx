722ac27f3a8821ba6f9fed54dc458fb428c27a2f
Gustavo Santos <z.gsantos20@gmail.com>
2021-11-14 20:23:15 -0100

    Changing some names and adding update animal screen

--- src/navigator/MainStack.tsx
+++ src/navigator/MainStack.tsx
@@ -8,6 +8,7 @@ import Settings from "../pages/Settings";
 import PaymentMethod from '../pages/PaymentMethod';
 import ViewAnimal from "../pages/ViewAnimal";
 import CreateAnimal from '../pages/CreateAnimal';
+import UpdateAnimal from "../pages/UpdateAnimal";
 
 interface AnimalInfoParams {
   age: string,
@@ -19,7 +20,7 @@ interface AnimalInfoParams {
   userid: number,
 }
 
-interface ViewAnimalParams {
+interface AnimalScreenParams {
   animalInfo: AnimalInfoParams
 }
 
@@ -28,8 +29,9 @@ export type RootStackParamList = {
   Login: undefined;
   Settings: undefined
   PaymentMethod: undefined,
-  CreateAnimal: undefined
-  ViewAnimal: ViewAnimalParams
+  CreateAnimal: undefined,
+  ViewAnimal: AnimalScreenParams,
+  UpdateAnimal: AnimalScreenParams,
 }
 
 const {Navigator, Group, Screen} = createStackNavigator<RootStackParamList>()
@@ -45,6 +47,7 @@ export default () => {
             <Screen options={{headerShown: false}} name="PaymentMethod" component={PaymentMethod}/>
             <Screen options={{headerShown: false}} name="CreateAnimal" component={CreateAnimal}/>
             <Screen options={{headerShown: false}} name="ViewAnimal" component={ViewAnimal}/>
+            <Screen options={{headerShown: false}} name="UpdateAnimal" component={UpdateAnimal}/>
           </Group>
         </Navigator>
     </NavigationContainer>
