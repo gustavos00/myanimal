439a1c3d088521527f61ba503dc35795237e947c
Gustavo Santos <z.gsantos20@gmail.com>
2021-11-25 02:45:41 -0100

    Changing to remove API url

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-let baseURL = false ? 'https://myanimalapi.herokuapp.com/api' : 'http://localhost:3000/api';
+let baseURL = true ? 'https://myanimalapi.herokuapp.com/api' : 'http://localhost:3000/api';
 
 const instance = axios.create({
   baseURL
