fe42bea260b733dd661ef96fccaffe52c4a7718e
Gustavo Santos <z.gsantos20@gmail.com>
2021-12-01 12:38:13 -0100

    Removing a file animal interface and importing a global interface

--- src/components/AnimalElement/index.tsx
+++ src/components/AnimalElement/index.tsx
@@ -3,58 +3,55 @@ import { useNavigation } from '@react-navigation/native';
 import { View, Text, StyleSheet, Image, TouchableOpacity } from 'react-native';
 
 import api from '../../api/api';
-
 import globalStyles from '../../assets/styles/global';
-import { showError } from '../../utils/error';
 
-interface AnimalDataProps {
-  age: string,
-  chipnumber: string,
-  id: number,
-  name: string,
-  photourl: string,
-  race: string,
-  userid: number,
-}
+import { showError } from '../../utils/error';
+import { AnimalInfoParams } from '../../interfaces/AnimalInfoParams';
 
 interface AnimalElementProps {
-  animalData: AnimalDataProps,
-  isEditing: boolean
+  animalData: AnimalInfoParams;
+  isEditing: boolean;
 }
 
-function AnimalElement({ animalData, isEditing } : AnimalElementProps) {
+function AnimalElement({ animalData, isEditing }: AnimalElementProps) {
   const navigation = useNavigation();
 
-  const deletingAnimal = async (id : number) => {
+  const deletingAnimal = async (id: number) => {
     let animalId = new FormData();
-    animalId.append('id', String(id))
+    animalId.append('id', String(id));
 
     try {
-    await api.post('/animal/delete', animalId);
-    } catch(e) {
-      showError('Error: ' + e, 'Apparently there was an error deleting this animal, try again')
-    } 
-  }
+      await api.post('/animal/delete', animalId);
+    } catch (e) {
+      showError(
+        'Error: ' + e,
+        'Apparently there was an error deleting this animal, try again'
+      );
+    }
+  };
 
   const updatingAnimal = () => {
-    navigation.navigate('UpdateAnimal' as never, {
-      animalInfo: animalData
-    } as never)
-  }
+    navigation.navigate(
+      'UpdateAnimal' as never,
+      {
+        animalInfo: animalData,
+      } as never
+    );
+  };
 
   return (
     <>
       <View style={styles.element}>
-        <Image source={{uri: animalData.photourl}} style={styles.icon}/>
+        <Image source={{ uri: animalData.imageUrl }} style={styles.icon} />
 
         <View style={styles.textContainer}>
           <Text style={styles.nameText}>{animalData.name}</Text>
-          <Text style={styles.raceText}>{animalData.race}</Text>
+          <Text style={styles.raceText}>{animalData.breed}</Text>
         </View>
 
         <View style={styles.editContainer}>
-          {isEditing && 
-            <>  
+          {isEditing && (
+            <>
               <TouchableOpacity onPress={updatingAnimal}>
                 <Image source={require('../../assets/img/edit.png')} />
               </TouchableOpacity>
@@ -63,7 +60,7 @@ function AnimalElement({ animalData, isEditing } : AnimalElementProps) {
                 <Image source={require('../../assets/img/delete.png')} />
               </TouchableOpacity>
             </>
-          }
+          )}
         </View>
       </View>
     </>
@@ -78,20 +75,20 @@ const styles = StyleSheet.create({
     backgroundColor: 'white',
     borderRadius: 15,
 
-    shadowColor: "#000",
+    shadowColor: '#000',
     shadowOffset: {
       width: 0,
       height: 0,
     },
     shadowOpacity: 0.13,
     shadowRadius: 10,
-    
+
     elevation: 4,
 
     flexDirection: 'row',
   },
 
-  icon: {
+  icon: {
     width: 84,
     height: 84,
     borderRadius: 15,
@@ -101,16 +98,16 @@ const styles = StyleSheet.create({
     marginLeft: 10,
     marginTop: 10,
   },
-   
+
   nameText: {
     color: globalStyles.black,
     fontSize: 18,
-    fontWeight: 'bold'
+    fontWeight: 'bold',
   },
 
   raceText: {
     fontSize: 16,
-    color: globalStyles.darkGray
+    color: globalStyles.darkGray,
   },
 
   editContainer: {
@@ -122,7 +119,7 @@ const styles = StyleSheet.create({
 
     justifyContent: 'space-evenly',
     alignItems: 'center',
-  }
-})
+  },
+});
 
 export default AnimalElement;
