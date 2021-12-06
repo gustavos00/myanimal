0b7ddf2ed2ec45ca7daccca74c1c864d3e923de9
Gustavo Santos <z.gsantos20@gmail.com>
2021-12-05 23:21:55 -0100

    Creating a container on Button component to centralize him

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,42 +1,52 @@
 import React, { ReactNode } from 'react';
-import { TouchableOpacity, StyleSheet, Text } from 'react-native';
+import { TouchableOpacity, StyleSheet, Text, View } from 'react-native';
 import globalStyles from '../../assets/styles/global';
 
 interface ButtonProps {
-  text: string,
-  handleClick: () => void
+  text: string;
+  handleClick: () => void;
 }
 
 function Button({ text, handleClick }: ButtonProps) {
   return (
     <>
-      <TouchableOpacity activeOpacity={.7} onPress={handleClick} style={styles.btn}>
-        <Text style={styles.btnText}>{text}</Text>
-      </TouchableOpacity>
+      <View style={styles.btnContainer}>
+        <TouchableOpacity
+          activeOpacity={0.7}
+          onPress={handleClick}
+          style={styles.btn}
+        >
+          <Text style={styles.btnText}>{text}</Text>
+        </TouchableOpacity>
+      </View>
     </>
   );
 }
 
 const styles = StyleSheet.create({
+  btnContainer: {
+    width: '100%',
+    marginTop: 20,
+    marginBottom: 20,
+
+    alignItems: 'center',
+  },
   btn: {
     width: 280,
     height: 50,
-    marginTop: 20,
-    marginBottom: 20,
-  
+
     justifyContent: 'center',
     alignItems: 'center',
 
     backgroundColor: globalStyles.mainColor,
-    borderRadius: 15
+    borderRadius: 15,
   },
 
   btnText: {
     fontSize: 24,
     fontWeight: 'bold',
-    color: globalStyles.white
-  }
-})
-
+    color: globalStyles.white,
+  },
+});
 
 export default Button;
