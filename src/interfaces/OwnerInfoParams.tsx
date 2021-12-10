8f339242509ff1f5c7940ca320d8f965c7523bc9
Gustavo Santos <z.gsantos20@gmail.com>
2021-12-09 23:04:15 -0100

    Adding owner address on ownerInfo interface

--- src/interfaces/OwnerInfoParams.tsx
+++ src/interfaces/OwnerInfoParams.tsx
@@ -1,4 +1,12 @@
 export interface OwnerInfoParams {
   email: string;
   phoneNumber: string;
+
+  latitude?: number
+  longitude?: number
+
+  streetName?: string
+  postalCode?: string
+  parishName?: string
+  locationName?: string
 }
