c8b6465526a1972accd0280c95a5bf9b183df12b
Gustavo Santos <z.gsantos20@gmail.com>
2021-12-17 14:00:22 -0100

    Fixing animal info interface

--- src/interfaces/AnimalInfoParams.tsx
+++ src/interfaces/AnimalInfoParams.tsx
@@ -1,14 +1,12 @@
 export interface AnimalInfoParams {
-  id: number;
-
-  name: string;
   age: string;
-  trackNumber: string;
-
   birthday: string;
   birthdayMonth: string;
-
-  imageUrl: string;
   breed: string;
-  userid: number;
+  idAnimal: number;
+  imageName: string;
+  imageUrl: string;
+  name: string;
+  trackNumber: string;
+  userIdUser: number;
 }
