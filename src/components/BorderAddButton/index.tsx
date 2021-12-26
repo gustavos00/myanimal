8b226c2e285bb4907953182ebaac8f171c147e14
Gustavo Santos <z.gsantos20@gmail.com>
2021-12-26 23:55:38 +0000

    Creating a circle button component

---
+++ src/components/BorderAddButton/index.tsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import { TouchableOpacity, Text, StyleSheet } from 'react-native';
+import globalStyles from '../../assets/styles/global';
+
+interface BorderAddButtonProps {
+  handleClick: () => void;
+}
+
+function BorderAddButton({ handleClick }: BorderAddButtonProps) {
+  return (
+    <>
+      <TouchableOpacity activeOpacity={.7} style={styles.button} onPress={handleClick}>
+        <Text style={styles.buttonText}>+</Text>
+      </TouchableOpacity>
+    </>
+  );
+}
+
+const styles = StyleSheet.create({
+  button: {
+    width: 50,
+    height: 50, 
+
+    borderWidth: 3,
+    borderColor: globalStyles.mainColor,
+    borderRadius: 50,
+    
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+
+  buttonText: {
+    width: 50,
+
+    fontSize: 35,
+    fontWeight: '400',
+    color: globalStyles.mainColor,
+    textAlign: 'center',
+  }
+})
+
+export default BorderAddButton;
