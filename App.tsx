dc43c24a92aee3525928486f02dcfaf5784e897e
Gustavo Santos <z.gsantos20@gmail.com>
2021-12-29 22:50:03 +0000

    Code format

--- App.tsx
+++ App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import MainStack from './src/navigator/MainStack';
-import { AuthProvider } from "./src/contexts/user";
+import { AuthProvider } from './src/contexts/user';
 
 export default function App() {
   return (
