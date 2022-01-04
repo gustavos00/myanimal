86e64b7b7eb5da9b5abec4f5fd057102c5d06e85
Gustavo Santos <z.gsantos20@gmail.com>
2022-01-04 00:55:35 +0000

    GGetting friends requests and generating elements

--- src/Pages/FriendsRequests.tsx
+++ src/Pages/FriendsRequests.tsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import { showError } from '../utils/error';
 
 import Background from '../components/Background';
 import BackgroundHeader from '../components/BackgroundHeader';
@@ -7,14 +8,35 @@ import Header from '../components/Header';
 import Loading from '../components/Loading';
 
 import api from '../api/api';
+import { FlatList, Text, View } from 'react-native';
+import AnimalElement from '../components/AnimalElement';
 
 function FriendsRequests() {
   const [loading, setLoading] = useState<boolean>();
+  const [friendsRequests, setFriendsRequests] = useState();
 
-  const handleFriendsRequest = async() => {
-    const response = await api.get('')
-  }
-  useEffect(() => {}, []);
+  const getAllFriendsRequests = async () => {
+    setLoading(true)
+    try {
+      const response = await api.get(`user/friend/get?id=1`);
+      setFriendsRequests(response.data);
+      setLoading(false)
+    } catch (e) {
+      setLoading(false);
+      return showError(
+        'Error: ' + e,
+        'Apparently there was an error, try again'
+      );
+    }
+  };
+
+  useEffect(() => {
+    const getFriendsRequests = async () => {
+      await getAllFriendsRequests();
+    };
+
+    getAllFriendsRequests();
+  }, []);
 
   return (
     <>
@@ -23,6 +45,19 @@ function FriendsRequests() {
       <Background>
         <>
           <BackgroundHeader text={'Friends requests'} />
+
+          <FlatList
+            data={friendsRequests}
+            renderItem={({index, item}) => {
+              console.log(index)
+              console.log('----')
+              return (
+                <View key={index}>
+                  <Text>{item.fromWho}</Text>
+                </View>
+              );
+            }}
+          />
         </>
       </Background>
 
