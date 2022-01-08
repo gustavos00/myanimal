a65e0c829ba51a1bbb9e46f041a11af6e5b89f25
Gustavo Santos <z.gsantos20@gmail.com>
2022-01-08 06:30:13 +0000

    Creating a state hook to manage expo token

--- src/contexts/auth.tsx
+++ src/contexts/auth.tsx
@@ -1,63 +1,54 @@
-import React, { createContext, useState } from 'react';
+import React, { createContext, useContext, useState } from 'react';
 import { GoogleSignInProps } from '../interfaces/GoogleSignInProps';
 import { showError } from '../utils/error';
 
 import * as auth from '../services/auth';
 import storage from '../utils/storage';
 import isEmpty from '../utils/isEmpty';
+import UserContext from './user';
 
 interface AuthContextData {
   token: string | void;
 
   googleSignIn: () => Promise<false | GoogleSignInProps | undefined>;
-  setToken: (token : string) => void
+  setToken: (token: string) => void;
+  setExpoToken: (expoToken: string) => void;
 }
 
 const AuthContext = createContext<AuthContextData>({} as AuthContextData);
 
 export function AuthProvider({ children }: any) {
   const [token, setToken] = useState<string | void>();
+  const [expoToken, setExpoToken] = useState<string | void>();
+
+  const { setAnimalData, setUser } = useContext(UserContext);
 
   const setTokenOnLocalStorage = async (token: string, salt: string) => {
     //Missing date
     try {
       await storage.save({ key: '@userAccess', data: { token, salt } });
     } catch (e) {
-      return showError(
-        'Error: ' + e,
-        'Apparently there was an error, try again'
-      );
+      return showError('Error: ' + e, 'Apparently there was an error, try again');
     }
   };
 
   const googleSignIn = async () => {
-    let tempObj = {};
+    const [haveAddressStatus, setHaveAddressStatus] = useState<boolean>();
 
     try {
       const response = await auth.GoogleSignIn();
       if (!response) return false;
-      console.log(response)
 
       setToken(response.token);
-    //   setAnimalData(googleResponse.animalData);
-    //   setUser(googleResponse);
-      await setTokenOnLocalStorage(
-        response.accessToken,
-        response.salt
-      );
-
-      tempObj = {
-        haveAddress: !isEmpty(response.userAddress),
-        isValid: response ? true : false,
-      };
+      setAnimalData(response.animalData);
+      setUser(response);
+      setHaveAddressStatus(!isEmpty(response.userAddress));
+      await setTokenOnLocalStorage(response.accessToken, response.salt);
     } catch (e) {
-      return showError(
-        'Error: ' + e,
-        'Apparently there was an error, try again'
-      );
+      return showError('Error: ' + e, 'Apparently there was an error, try again');
     }
 
-    return tempObj;
+    return { haveAddress: haveAddressStatus };
   };
 
   return (
@@ -65,7 +56,8 @@ export function AuthProvider({ children }: any) {
       value={{
         token,
         googleSignIn,
-        setToken
+        setToken,
+        setExpoToken,
       }}
     >
       {children}
