b8403e5b351c4070222fc56433cd79f606ec4ba8
Gustavo Santos <z.gsantos20@gmail.com>
2022-01-13 02:59:13 +0000

    Creating function to accept friends request

--- src/contexts/friends.tsx
+++ src/contexts/friends.tsx
@@ -1,11 +1,14 @@
-import React, { createContext, Dispatch, SetStateAction, useState } from 'react';
+import React, { createContext, useState } from 'react';
 import { FriendsData } from '../types/FriendsData';
+
+import api from '../api/api';
 
 interface FriendsContextData {
   pendingFriends: Array<FriendsData> | undefined;
   acceptedFriends: Array<FriendsData> | undefined;
   handlePendingFriends: (data: Array<FriendsData>) => void;
-  acceptFriendRequest: (index: number) => void;
+  handleAcceptedFriends: (data: Array<FriendsData>) => void;
+  acceptFriendsRequest: (index: number) => void;
 }
 
 const FriendsContext = createContext<FriendsContextData>({} as FriendsContextData);
@@ -18,25 +21,44 @@ export function FriendsProvider({ children }: any) {
     setPendingFriends(data);
   };
 
-  const acceptFriendRequest = (index: number) => {
-    if (!pendingFriends) return;
+  const handleAcceptedFriends = (data: Array<FriendsData>) => {
+    setAcceptedFriends(data);
+  };
+
+  const acceptFriendsRequest = async (index: number) => {
+    if (!pendingFriends) return console.log('Pending friends dont exist');
 
     const tempPendingArray = pendingFriends;
-    const tempAcceptedArray = acceptedFriends;
     const tempObj = tempPendingArray[index];
+    const idFriendsElement = tempPendingArray[index].idfriends;
 
     tempObj.status = 'Accepted';
-    tempPendingArray.slice(index)
-    tempAcceptedArray.push(tempObj);
+    tempPendingArray.splice(index, 1);
 
-    setPendingFriends(tempPendingArray)
-    setAcceptedFriends(tempAcceptedArray);
-    console.log(acceptedFriends)
+    setPendingFriends(tempPendingArray);
+    setAcceptedFriends((acceptedFriends) => {
+      const alreadyContainsStatus =
+        acceptedFriends.filter((e) => e.idfriends === acceptedFriends[index].idfriends).length > 0;
+
+      if (alreadyContainsStatus) {
+        return [...acceptedFriends];
+      }
+      return [...(acceptedFriends ?? []), tempObj];
+    });
+
+    const response = await api.get(`/user/friends/accept?id=${idFriendsElement}`);
+    console.log(response.data);
   };
 
   return (
     <FriendsContext.Provider
-      value={{ pendingFriends, acceptedFriends, handlePendingFriends, acceptFriendRequest }}
+      value={{
+        pendingFriends,
+        acceptedFriends,
+        handlePendingFriends,
+        handleAcceptedFriends,
+        acceptFriendsRequest,
+      }}
     >
       {children}
     </FriendsContext.Provider>
