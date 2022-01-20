0b82d4882f736065b60b936402326d0dcf94e779
Gustavo Santos <z.gsantos20@gmail.com>
2022-01-20 22:49:44 +0000

    Adding fingerprint to friends data interface

--- src/types/FriendsData.tsx
+++ src/types/FriendsData.tsx
@@ -3,6 +3,7 @@ import { UserData } from "./UserData"
 export interface FriendsData {
    idfriends: number,
    status: string,
+   fingerprint: string,
    fromWho: number,
    toWhom: number,
    fromWhoFk: UserData
