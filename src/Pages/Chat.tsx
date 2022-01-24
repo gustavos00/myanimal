b9cca07d745f5c0c53bf011bbb33e69c088dfa5e
Gustavo Santos <z.gsantos20@gmail.com>
2022-01-24 02:40:00 +0000

    Adding a to-do comment

--- src/Pages/Chat.tsx
+++ src/Pages/Chat.tsx
@@ -37,6 +37,7 @@ export default function Chat() {
 
   useEffect(() => {
     const getMessages = async () => {
+      // TO DO -> Get device ID
       const unsubscribe = await Firestore.onSnapshot(chatsRef, (doc) => {
         const messages = [] as Array<IMessages>;
         const docArray = doc.docChanges();
