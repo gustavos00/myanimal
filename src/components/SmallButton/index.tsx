3d5325a6d67beeee46ec158a17292215795e0651
Gustavo Santos <z.gsantos20@gmail.com>
2022-02-20 20:05:41 -0100

    Creating a small button component to apply on veterinarian profile

---
+++ src/components/SmallButton/index.tsx
@@ -0,0 +1,56 @@
+import React from 'react';
+
+import { Text, StyleSheet, TouchableOpacity } from 'react-native';
+import globalStyles from '../../assets/styles/global';
+import Background from '../Background';
+
+interface SmallButtonProps {
+  handleClick: () => void;
+
+  text: string;
+  borderColor?: string;
+  textColor?: string;
+  backgroundColor?: string;
+}
+
+function SmallButton({
+  handleClick,
+  text,
+  borderColor,
+  textColor,
+  backgroundColor,
+}: SmallButtonProps) {
+  return (
+    <TouchableOpacity
+      activeOpacity={0.7}
+      onPress={handleClick}
+      style={[
+        styles.button,
+        !!borderColor && { borderColor, borderWidth: 2, },
+        !!backgroundColor && { backgroundColor },
+      ]}
+    >
+      <Text style={[styles.title, !!textColor && { color: textColor }]}>{text}</Text>
+    </TouchableOpacity>
+  );
+}
+
+const styles = StyleSheet.create({
+  button: {
+    width: globalStyles.fullDeviceWidth * 0.38,
+    height: 50,
+
+    justifyContent: 'center',
+    alignItems: 'center',
+
+    borderRadius: 15,
+  },
+
+  title: {
+    fontWeight: 'bold',
+    fontSize: 24,
+    color: '#000',
+  },
+});
+
+export default SmallButton;
